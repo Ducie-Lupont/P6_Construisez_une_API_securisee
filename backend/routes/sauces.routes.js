6fd04e89f57454912308114ae6d202ada4da6733
Ducie-Lupont <dupont.lucie.018@gmail.com>
2022-07-03 12:07:06 +0200

    ordre routes

--- backend/routes/sauces.routes.js
+++ backend/routes/sauces.routes.js
@@ -6,11 +6,11 @@ const auth = require("../middleware/auth.middleware");
 const multer = require("../middleware/multer-config.middleware");
 
 //Quelles routes emprunter selon le type de requêtes    /!\ l'ordre des éléments entre parenthèses est important /!\//
+router.get("/", auth, saucesCtrl.findSauces);
+router.get("/:id", auth, saucesCtrl.findOneSauces);
 router.post("/", auth, multer, saucesCtrl.createSauces);
 router.put("/:id", auth, multer, saucesCtrl.modifySauces);
 router.delete("/:id", auth, saucesCtrl.deleteSauces);
-router.get("/:id", auth, saucesCtrl.findOneSauces);
-router.get("/", auth, saucesCtrl.findSauces);
 router.post("/:id/like", auth, saucesCtrl.userLikeSauces);
 
 module.exports = router;
